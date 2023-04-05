69c21132a2babb96cbf953064f8451185e58fe80
giahuy16404 <phamhuy1642004@gmail.com>
2023-04-05 15:07:27 +0700

    src

---
+++ Module3Data/ss02_database_design_and_creation/ss02_quanlybanhang.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE ss02_quanlybanhang;
+USE ss02_quanlybanhang;
+CREATE TABLE customer(
+	cid INT AUTO_INCREMENT PRIMARY KEY,
+    cname NVARCHAR(50) NOT NULL,
+    cage DATE NOT NULL
+    );
+CREATE TABLE orders(
+	oid INT AUTO_INCREMENT PRIMARY KEY,
+    cid INT,
+    odate DATE NOT NULL,
+    ototailprice DECIMAL NOT NULL,
+    FOREIGN KEY (cid) REFERENCES customer(cid)
+    );
+CREATE TABLE product(
+	pid INT AUTO_INCREMENT PRIMARY KEY,
+    pname NVARCHAR(50) NOT NULL,
+    pprice DECIMAL NOT NULL
+    );
+CREATE TABLE orderdetail(
+	oid INT,
+    pid INT,
+    odqty NVARCHAR(50) NOT NULL,
+    FOREIGN  KEY (oid) REFERENCES orders(oid),
+    FOREIGN KEY (pid) REFERENCES product(pid)
+    );
+SHOW TABLES;
+    
+
+    
+    
