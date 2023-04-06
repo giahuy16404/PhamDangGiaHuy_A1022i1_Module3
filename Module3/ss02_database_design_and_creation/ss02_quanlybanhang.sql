70b2eb56846312d4d6cfa26b5182885c24dd0d3f
giahuy16404 <phamhuy1642004@gmail.com>
2023-04-06 10:41:41 +0700

    src

--- Module3/ss02_database_design_and_creation/ss02_quanlybanhang.sql
+++ Module3/ss02_database_design_and_creation/ss02_quanlybanhang.sql
@@ -29,15 +29,23 @@ CREATE TABLE orderdetail(
     VALUES (N'HUY','2004-04-16');
     INSERT INTO customer(cname,cage)
     VALUES (N'OANH','2004-03-23');
+    INSERT customer(cname,cage)
+    VALUE (N'BO','2011-07-20');
 
     INSERT INTO product(pname,pprice)
     VALUES (n'BÀNPHÍM',120000);
     INSERT INTO product(pname,pprice)
     VALUES (N'CHUỘT',50000);
-    
+
 SHOW TABLES;
 SELECT * FROM customer;
 SELECT * FROM orders;
 SELECT * FROM product;
 SELECT * FROM orderdetail;
+BEGIN;
+ROLLBACK;
+COMMIT;
+DELETE FROM customer WHERE cid = 1;
+UPDATE customer set cname = 'Vy' WHERE cid = 5;
+
 
