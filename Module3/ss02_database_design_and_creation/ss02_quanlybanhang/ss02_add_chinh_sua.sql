abc9956eb9048b39ca5bb190c313b4505e99286c
giahuy16404 <phamhuy1642004@gmail.com>
2023-04-06 13:59:15 +0700

    src

---
+++ Module3/ss02_database_design_and_creation/ss02_quanlybanhang/ss02_add_chinh_sua.sql
@@ -0,0 +1,17 @@
+USE ss02_quanlybanhang;
+    ALTER TABLE orders MODIFY ototailprice INT NOT NULL;
+    ALTER TABLE product MODIFY pprice INT NOT NULL;
+SHOW TABLES;
+SELECT * FROM customer;
+SELECT * FROM orders;
+SELECT * FROM product;
+SELECT * FROM orderdetail;
+BEGIN;
+ROLLBACK;
+COMMIT;
+DELETE FROM orders;
+DELETE FROM customer;
+UPDATE customer 
+set cname = 'Vy' WHERE cid = 5;
+ALTER TABLE customer auto_increment = 1;
+ALTER TABLE orders auto_increment = 1;
---
+++ Module3/ss02_database_design_and_creation/ss02_quanlybanhang/ss02_add_data.sql
@@ -0,0 +1,27 @@
+USE ss02_quanlybanhang;
+    INSERT INTO customer(cname,cage)VALUES (N'HUY','2004-04-16');
+    INSERT INTO customer(cname,cage)VALUES (N'OANH','2004-03-23');
+    INSERT customer(cname,cage)VALUE (N'BO','2011-07-20');
+	INSERT INTO customer(cname,cage)VALUES (N'HUY','2004-04-16');
+    INSERT INTO customer(cname,cage)VALUES (N'OANH','2004-03-23');
+    INSERT customer(cname,cage)VALUE (N'BO','2011-07-20');
+	INSERT INTO customer(cname,cage)VALUES (N'HUY','2004-04-16');
+    INSERT INTO customer(cname,cage)VALUES (N'OANH','2004-03-23');
+    INSERT customer(cname,cage)VALUE (N'BO','2011-07-20');
+	INSERT INTO customer(cname,cage)VALUES (N'HUY','2004-04-16');
+    INSERT INTO customer(cname,cage)VALUES (N'OANH','2004-03-23');
+    INSERT customer(cname,cage)VALUE (N'BO','2011-07-20');
+    
+    INSERT orders(cid,odate,ototailprice) VALUE (1,'2004-03-23',100);
+    INSERT orders(cid,odate,ototailprice) VALUE (2,'2004-03-23',200);
+    INSERT orders(cid,odate,ototailprice) VALUE (3,'2004-03-23',300);
+    INSERT orders(cid,odate,ototailprice) VALUE (4,'2004-03-23',400);
+    INSERT orders(cid,odate,ototailprice) VALUE (5,'2004-03-23',500);
+    INSERT orders(cid,odate,ototailprice) VALUE (6,'2004-03-23',600);
+    INSERT orders(cid,odate,ototailprice) VALUE (7,'2004-03-23',700);
+    INSERT orders(cid,odate,ototailprice) VALUE (8,'2004-03-23',800);
+    
+	INSERT INTO product(pname,pprice)
+    VALUES (n'BÀNPHÍM',120000);
+    INSERT INTO product(pname,pprice)
+    VALUES (N'CHUỘT',50000);
